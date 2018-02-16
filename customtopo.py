a6f2860c800daf1308e1ac7aedf4be9a35eb3344
shivani305 <shivaniguptha305@gmail.com>
2018-02-16 15:58:19 -0800

    Update customtopo.py

--- customtopo.py
+++ customtopo.py
@@ -12,20 +12,20 @@ class MyTopo( Topo ):
         # Add hosts and switches
         leftHost = self.addHost( 'h1' )
         rightHost = self.addHost( 'h2' )
-        thirdHost = self.addHost( 'h3' )
-        fourthHost = self.addHost( 'h4' )
+       # thirdHost = self.addHost( 'h3' )
+        #fourthHost = self.addHost( 'h4' )
         leftSwitch = self.addSwitch( 's1' )
         rightSwitch = self.addSwitch( 's2' )
-        thirdswitch = self.addSwitch( 's3' )
-        fourthswitch = self.addSwitch( 's4' )
+        #thirdswitch = self.addSwitch( 's3' )
+        #fourthswitch = self.addSwitch( 's4' )
         # Add links
         self.addLink( leftHost, leftSwitch )
         self.addLink( leftSwitch, rightSwitch )
         self.addLink( rightSwitch, rightHost )
-        self.addLink( rightSwitch, thirdSwitch )
-        self.addLink( thirdSwitch, thirdHost )
-        self.addLink( rightSwitch, fourthSwitch )
-        self.addLink( fourthSwitch, fourthHost )
+       # self.addLink( rightSwitch, thirdSwitch )
+        #self.addLink( thirdSwitch, thirdHost )
+        #self.addLink( rightSwitch, fourthSwitch )
+        #self.addLink( fourthSwitch, fourthHost )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
